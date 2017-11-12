c198884dca0ff00ed940941476a1d12d3bff568a
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-12 21:52:29 +0000

    adding baseUrl constant

--- src/services/github.service.js
+++ src/services/github.service.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://api.github.com/search/repositories'
+const baseUrl ='https://api.github.com/search/repositories' ;
 
 const githubService ={
 	search(query,criteria='stars' , order ='desc', page = 0 ){
-		console.log(`https://api.github.com/search/repositories?q='${query}'&&sort='${criteria}'&&order='${order}'&&page=${page}`);
 		return new Promise((resolve)=> {
-			axios.get(`https://api.github.com/search/repositories?
-				q=${query}&&sort=${criteria}&&order=${order}&&page=${page}`)
+			axios.get(`${baseUrl}?q=${query}&&sort=${criteria}&&order=${order}&&page=${page}`)
 			.then(res => {
               		resolve(res.data)
 			})
