33062e299f06ca455c23876f27acfceb31024140
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-12 15:35:18 +0000

    adding github service

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <app-header @queryEmited="submitQuery"/>
     <filter-component @criteriaChanged="filterRepositorie"></filter-component>
     <div class="container">
-    <repositories-component  :repositories="items"></repositories-component>  
+      <repositories-component  :repositories="items"></repositories-component>  
     </div>
     
     <app-footer/>
@@ -15,80 +15,14 @@ import AppHeader from './components/AppHeader'
 import AppFooter from './components/AppFooter'
 import filterComponent from './components/filterComponent'
 import RepositoriesComponent from './components/RepositoriesComponent'
+import githubService from './services/github.service'
 
 export default {
   name: 'app',
   data()  {
     return {
-  "total_count": 40,
-  "incomplete_results": false,
-  "items": [
-    {
-      "id": 3081286,
-      "name": "Tetris",
-      "full_name": "dtrupenn/Tetris",
-      "owner": {
-        "login": "dtrupenn",
-        "id": 872147,
-        "avatar_url": "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/dtrupenn",
-        "received_events_url": "https://api.github.com/users/dtrupenn/received_events",
-        "type": "User"
-      },
-      "private": false,
-      "html_url": "https://github.com/dtrupenn/Tetris",
-      "description": "A C implementation of Tetris using Pennsim through LC4",
-      "fork": false,
-      "url": "https://api.github.com/repos/dtrupenn/Tetris",
-      "created_at": "2012-01-01T00:31:50Z",
-      "updated_at": "2013-01-05T17:58:47Z",
-      "pushed_at": "2012-01-01T00:37:02Z",
-      "homepage": "",
-      "size": 524,
-      "stargazers_count": 1,
-      "watchers_count": 1,
-      "language": "Assembly",
-      "forks_count": 0,
-      "open_issues_count": 0,
-      "master_branch": "master",
-      "default_branch": "master",
-      "score": 10.309712
-    },
-    {
-      "id": 3081286,
-      "name": "Tetris",
-      "full_name": "dtrupenn/Tetris",
-      "owner": {
-        "login": "dtrupenn",
-        "id": 872147,
-        "avatar_url": "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/dtrupenn",
-        "received_events_url": "https://api.github.com/users/dtrupenn/received_events",
-        "type": "User"
-      },
-      "private": false,
-      "html_url": "https://github.com/dtrupenn/Tetris",
-      "description": "A C implementation of Tetris using Pennsim through LC4",
-      "fork": false,
-      "url": "https://api.github.com/repos/dtrupenn/Tetris",
-      "created_at": "2012-01-01T00:31:50Z",
-      "updated_at": "2013-01-05T17:58:47Z",
-      "pushed_at": "2012-01-01T00:37:02Z",
-      "homepage": "",
-      "size": 524,
-      "stargazers_count": 1,
-      "watchers_count": 1,
-      "language": "Assembly",
-      "forks_count": 0,
-      "open_issues_count": 0,
-      "master_branch": "master",
-      "default_branch": "master",
-      "score": 10.309712
+      repositories : []
     }
-  ]
-}
   },
   components: {
     AppHeader,
@@ -101,7 +35,10 @@ export default {
       console.log(criteria)
     },
     submitQuery(query) {
-      console.log("search for "+query)
+      githubService.search(query).then(data => {
+        console.log(data);
+        this.repositories = data;
+      })
     }
   }
 }
@@ -116,7 +53,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}*/
-</style>
+  }*/
+  </style>
 
-<style type="text/css" src="./assets/css/main.css"></style>
+  <style type="text/css" src="./assets/css/main.css"></style>
---
+++ src/services/github.service.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.github.com/search/repositories'
+
+const githubService ={
+	search(query,criteria='stars'){
+		return new Promise((resolve)=> {
+			axios.get(`https://api.github.com/search/repositories/q=${query}&code=' '`)
+			.then(res => {
+              		resolve(res.data)
+			})
+		})
+
+	}
+}
+export default githubService
