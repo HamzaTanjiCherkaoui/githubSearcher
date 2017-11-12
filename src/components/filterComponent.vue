171f0cf7ebcdf08a1a87b8f0d8b1e312e63951bf
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-12 17:43:04 +0000

    adding isActive to show the active filter

--- src/components/filterComponent.vue
+++ src/components/filterComponent.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="filter-section">
 		<div class="controls">
-			<button class="box primary-bg icon-btn" @click="filterBy('stars')" title="stars"><i class="fa fa-star"></i></button>
-			<button class="box primary-bg icon-btn" @click="filterBy('forks')" title="forks"><i class="fa fa-code-fork"></i></button>
-			<button class="box primary-bg icon-btn" @click="filterBy('updated')" title="activity"><i class="fa fa-bolt" ></i></button>
+			<button :class="{ 'secondary-bg': isActive=='stars'  }" class="box primary-bg  icon-btn" @click="filterBy('stars')" title="stars"><i class="fa fa-star"></i></button>
+			<button :class="{ 'secondary-bg': isActive=='forks' }" class="box primary-bg  icon-btn" @click="filterBy('forks')" title="forks"><i class="fa fa-code-fork"></i></button>
+			<button :class="{ 'secondary-bg': isActive=='updated' }" class="box primary-bg  icon-btn" @click="filterBy('updated')" title="activity"><i class="fa fa-bolt" ></i></button>
 		</div>
 	</div>
 
@@ -12,8 +12,12 @@
 <script>
 export default {
 	name: 'filter-component',
+	data() {
+		return { isActive : 'stars'}
+	},
 	methods : {
 		filterBy(criteria) {
+			this.isActive = criteria
 			this.$emit('criteriaChanged' , criteria);
 		}
 	}
